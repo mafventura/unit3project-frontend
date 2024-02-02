5ff13ff82cb65bff081371d27ec4b24bba256519
username <youremail@domain.com>
2024-02-02 12:10:56 +0000

    quicks

---
+++ src/Components/Quicks.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Button, Card } from 'react-bootstrap'
+
+export default function QuicksModal({water, sleep, mood, quote}) {
+  const classNames = []
+
+
+
+
+  return (
+    <Card className={classNames.join(" ")}>
+            <Card.Body>
+    
+            </Card.Body>
+
+    </Card>
+    
+ )
+}
