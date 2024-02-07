e15bd6c77efe3ea185930df99dd103b036c34f78
Mafalda <mafalda.ventura@live.com.pt>
2024-02-07 21:30:34 +0000

    styled sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -5,32 +5,37 @@ import {
   CDBSidebarHeader,
   CDBSidebarMenu,
   CDBSidebarMenuItem,
-} from 'cdbreact';
-import { NavLink } from 'react-router-dom'
-import { Button } from 'react-bootstrap'
-import AllToDos from './todos/AllToDos';
-import { Routes, Route } from 'react-router-dom'
+} from "cdbreact";
+import { NavLink } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { CiLogout } from "react-icons/ci";
 
 export default function Sidebar({ handleLogout, user }) {
   return (
-    <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
-      <CDBSidebar textColor="#fff" backgroundColor="#3C7D54">
+    <div style={{ display: "flex", height: "100vh", overflow: "scroll initial" }}>
+      <CDBSidebar textColor="#F4F4F1" backgroundColor="#3C7D54">
         <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
-          <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}>
-            JOURNEY
-          </a>
-          <p style={{
-            color: 'rgb(233,237,200,0.7)',
-            margin: '5px 0 0',
-            fontFamily: 'fantasy'
-            }}>
+          <div style={{display: 'flex', flexDirection: 'column', alignItems: 'start'}}>
+            <img
+              src={"https://i.imgur.com/xlUSgOl.png"}
+              alt=""
+              style={{ width: "180px", marginLeft: '-20px' }}
+            />
+            <p
+              style={{
+                color: "#F4F4F1",
+                margin: "5px 0 0",
+                fontFamily: "PT Serif, serif",
+              }}
+            >
               Hello, {user?.given_name}
-          </p>
+            </p>
+          </div>
         </CDBSidebarHeader>
 
         <CDBSidebarContent className="sidebar-content">
           <CDBSidebarMenu>
-            <NavLink to='/'>
+            <NavLink to="/">
               <CDBSidebarMenuItem icon="home">Home</CDBSidebarMenuItem>
             </NavLink>
             <NavLink to="/dailies">
@@ -44,23 +49,23 @@ export default function Sidebar({ handleLogout, user }) {
             </NavLink>
           </CDBSidebarMenu>
         </CDBSidebarContent>
-        <CDBSidebarFooter style={{ textAlign: 'center' }}>
-          <div
-            className="sidebar-btn-wrapper"
-            
-          >
-            <Button variant="secondary" onClick={handleLogout} 
+        <CDBSidebarFooter style={{ textAlign: "center" }}>
+          <div className="sidebar-btn-wrapper">
+            <Button
+              variant="secondary"
+              onClick={handleLogout}
               style={{
-                width: '100%',
-                background: 'rgb(233, 237, 200, 0.2)',
-                border: 'none',
-                borderRadius: '0'
-            }}>
-              logout
+                width: "100%",
+                background: "rgb(233, 237, 200, 0.2)",
+                border: "none",
+                borderRadius: "0",
+              }}
+            >
+              <CiLogout style={{ marginRight: "5px" }} />
             </Button>
           </div>
         </CDBSidebarFooter>
       </CDBSidebar>
     </div>
-  )
+  );
 }
