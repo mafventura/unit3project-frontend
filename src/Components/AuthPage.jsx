ff353dc4dc8443bee5d02576fe24aa8a6fa995ac
Mafalda <mafalda.ventura@live.com.pt>
2024-02-07 19:08:37 +0000

    styled AuthPage

--- src/Components/AuthPage.jsx
+++ src/Components/AuthPage.jsx
@@ -1,34 +1,21 @@
-import { useCallback } from 'react'
-import { Button } from 'react-bootstrap'
+import { useCallback } from "react";
+import { Button, Container } from "react-bootstrap";
 
 export default function AuthPage({ user, setUser, handleLogout, getUSer }) {
-
     const googleAuth = useCallback(() => {
-        window.open(
-            `${process.env.REACT_APP_BACKEND_URL}/auth/google/callback`,
-            '_self'
-        )
-    }, [])
+    window.open(`${process.env.REACT_APP_BACKEND_URL}/auth/google/callback`, "_self");
+    }, []);
 
-    return(
-        <div style={{
-            height: '100%',
-            display: 'flex',
-            background: 'rgb(233, 237, 200)',
-            flexDirection: 'row',
-            justifyContent: "center",
-            alignItems: "center"
-        }}>
-            <div className="btn-logo-wrapper" style= {{
-                height : '60%',
-                width: '30%',
-                border: '1px solid black',
-                display: 'flex',
-                justifyContent: "center",
-                alignItems: "center"
-            }}>
-            <Button variant="outline-secondary" onClick={googleAuth} >Login with google</Button>
-            </div> 
-        </div>
-    )
+    return (
+    <div
+        style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}
+    >
+        <Container className="d-flex flex-column align-items-center">
+            <img src="https://i.imgur.com/xlUSgOl.png" alt="journee-logo" style={{ width: "500px" }} />
+            <Button variant="success" style={{ width: "200px" }} onClick={googleAuth}>
+                Login
+            </Button>
+        </Container>
+    </div>
+    );
 }
